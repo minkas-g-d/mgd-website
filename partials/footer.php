5a9ea03529b51f2f1deb38e91948d9b0291bb09e
Mina Dodunekova <m.dodunekova@up2.nl>
2017-05-23 15:21:19 +0300

    Add partials php files

---
+++ partials/footer.php
@@ -0,0 +1,25 @@
+<?php
+	$markup = 
+	'<footer id="footer">
+		<section>
+			<h2>Всички продукти</h2>
+			<ul>
+				<li><a href="mineralni-i-trapezni-vodi.html">Минерални и трапезни води</a></li>
+				<li><a href="#">Консервирани и пакетирани храни</a></li>
+				<li><a href="#">Подправки и сушени продукти</a></li>
+			</ul>
+		</section>
+		<section>
+			<h2>Контакти:</h2>
+			<dl class="alt">
+				<dt>Телефон</dt>
+				<dd>0899 818 762</dd>
+				<dt>Email</dt>
+				<dd><a href="#">mina_eood@abv.bg</a></dd>
+			</dl>
+		</section>
+		<p class="copyright">&copy; Мина-Г ЕООД. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+	</footer>';
+
+		echo $markup;
+?>
